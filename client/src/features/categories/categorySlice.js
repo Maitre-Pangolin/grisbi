0bffd844f166583451bd59bf7a3a2eb18082ff8d
Maitre-Pangolin <martin.wasselet@gmail.com>
2021-12-17 18:32:58 -0500

    Import category icons

--- client/src/features/categories/categorySlice.js
+++ client/src/features/categories/categorySlice.js
@@ -1,6 +1,32 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { getCategories } from "../../api/API";
 
+import HomeIcon from "@mui/icons-material/Home";
+import DirectionsBikeIcon from "@mui/icons-material/DirectionsBike";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import RestaurantIcon from "@mui/icons-material/Restaurant";
+import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
+import SportsTennisIcon from "@mui/icons-material/SportsTennis";
+import LocalMoviesIcon from "@mui/icons-material/LocalMovies";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import CheckroomIcon from "@mui/icons-material/Checkroom";
+import WineBarIcon from "@mui/icons-material/WineBar";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+
+export const categoryIcons = [
+  <AttachMoneyIcon />,
+  <HomeIcon />,
+  <DirectionsBikeIcon />,
+  <ShoppingBasketIcon />,
+  <RestaurantIcon />,
+  <LocalHospitalIcon />,
+  <SportsTennisIcon />,
+  <LocalMoviesIcon />,
+  <SportsEsportsIcon />,
+  <CheckroomIcon />,
+  <WineBarIcon />,
+];
+
 const fetchCategories = createAsyncThunk("category/getCategory", async () => {
   const response = await getCategories();
   return response.data;
@@ -8,16 +34,19 @@ const fetchCategories = createAsyncThunk("category/getCategory", async () => {
 
 const categorySlice = createSlice({
   name: "categories",
-  initialState: {},
+  initialState: [],
   reducers: {},
   extraReducers: (builder) => {
     // Add reducers for additional action types here, and handle loading state as needed
-    builder.addCase(fetchCategories.fulfilled, (state, action) => ({
-      ...action.payload,
-    }));
+    builder.addCase(
+      fetchCategories.fulfilled,
+      (state, action) => action.payload
+    );
   },
 });
 
 export default categorySlice.reducer;
 export { fetchCategories };
 export const selectAllCategories = (state) => state.categories;
+export const selectCategoryById = (categoryID) => (state) =>
+  state.categories.find(({ id }) => id === categoryID);
