83a701052bdd057a473777f5a64a800400c5abb6
Maitre-Pangolin <martin.wasselet@gmail.com>
2022-01-04 12:20:26 -0500

    Fix homepage style

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,54 +1,51 @@
 import React from "react";
-import { Button, Typography } from "@mui/material";
+import { Button, Typography, Paper, Container } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <>
-      <div
-        style={{
+    <Paper
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexWrap: "wrap",
+        maxWidth: "1000px",
+        margin: { sm: " 10px auto", md: " 200px auto" },
+        padding: "30px 10px",
+      }}>
+      <img
+        alt='Blue rupee'
+        src='/android-chrome-512x512.png'
+        style={{ width: "300px", paddingBottom: "40px" }}
+      />
+      <Container
+        sx={{
+          width: "max-content",
           display: "flex",
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
+          flexDirection: "column",
+          alignItems: { xs: "center", md: "normal" },
+          gap: "40px",
         }}>
-        <img
-          alt='Blue rupee'
-          src='/android-chrome-512x512.png'
-          style={{ width: "300px" }}
-        />
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            padding: "20px",
-            justifyContent: "space-around",
-            alignItems: "flex-end",
-            minWidth: "600px",
-            height: "300px",
-          }}>
-          <Typography variant='h4'>
-            Tired of losing track of your rupees ?
-          </Typography>
-          <Typography variant='h4' align='right'>
-            <strong style={{ color: "#3464eb" }}>Gri$bi</strong> is here for you
-            !
-          </Typography>
-          <Button
-            size='large'
-            variant='contained'
-            sx={{
-              width: "45%",
-            }}
-            onClick={() => navigate("/signup")}>
-            Sign Up Now
-          </Button>
-        </div>
-      </div>
-    </>
+        <Typography
+          variant='h4'
+          sx={{ fontSize: { xs: "1.5em", md: "2em" }, textAlign: "center" }}>
+          Tired of losing track of your rupees ?
+        </Typography>
+        <Typography variant='h4' sx={{ fontSize: { xs: "1.5em", md: "2em" } }}>
+          <strong style={{ color: "#3464eb" }}>Gri$bi</strong> is here for you !
+        </Typography>
+        <Button
+          size='large'
+          variant='contained'
+          sx={{}}
+          onClick={() => navigate("/signup")}>
+          Sign Up Now
+        </Button>
+      </Container>
+    </Paper>
   );
 };
 
